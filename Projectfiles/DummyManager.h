122efc53ee01c01d9974409817258f7319856464
notoroid <noto@irimasu.com>
2013-01-26 17:28:06 +0900

    デモモード(iPadデバッグ向け)を追加

---
+++ Projectfiles/DummyManager.h
@@ -0,0 +1,13 @@
+//
+//  DummyManager.h
+//  MultipleSession
+//
+//  Created by 能登 要 on 13/01/26.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DummyManager : NSObject
+
+@end
---
+++ Projectfiles/DummyManager.m
@@ -0,0 +1,13 @@
+//
+//  DummyManager.m
+//  MultipleSession
+//
+//  Created by 能登 要 on 13/01/26.
+//
+//
+
+#import "DummyManager.h"
+
+@implementation DummyManager
+
+@end
